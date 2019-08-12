cdeaf04207db02f8b9911fac7c6b1dbbe23f5b77
Katsuya Hyodo <rmsdh122@yahoo.co.jp>
2019-08-12 20:26:19 +0900

    Update download.sh

--- armv7l/Debian_Buster/v1.3.0-alpha/download.sh
+++ armv7l/Debian_Buster/v1.3.0-alpha/download.sh
@@ -5,5 +5,5 @@ CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1HhKU8_LipyYSPYE4X9XxXn8RZxGpkswu" -o julia-1.3.0-alpha.tar.gz
 tar -zxvf julia-1.3.0-alpha.tar.gz
 rm julia-1.3.0-alpha.tar.gz
-echo "Finish!!"
+echo Finish
 
